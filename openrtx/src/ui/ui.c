547fc6d6bd024e132730fa22affbd5a0d94cd380
vk7js <58905135+vk7js@users.noreply.github.com>
2022-09-06 12:18:45 +1000

    Initial beep implementation. Function latch will play high beep when latched and low beep when latch times out. Menus will play a high beep when you arrow through them and a low beep when you reach the first item. All keys should play a generic beep sound but this has not been implemented yet.

--- openrtx/src/ui/ui.c
+++ openrtx/src/ui/ui.c
@@ -279,13 +279,13 @@ static void ReleaseFunctionLatchIfNeeded()
         return;
 
     functionLatchTimer = 0;
-    // Play beep for function latch release.
+    vp_beep(200, 500);
 }
 
 static void SetFunctionLatchTimer()
 {
     functionLatchTimer= getTick() + FUNCTION_LATCH_TIMEOUT;
-    // Play beep for set function latch.
+    vp_beep(800, 500);
 }
 
 static bool FunctionKeyIsLatched()
@@ -978,6 +978,7 @@ static void _ui_menuUp(uint8_t menu_entries)
         ui_state.menu_selected -= 1;
     else
         ui_state.menu_selected = menu_entries - 1;
+    vp_playMenuBeepIfNeeded(ui_state.menu_selected==0);
 }
 
 static void _ui_menuDown(uint8_t menu_entries)
@@ -986,6 +987,7 @@ static void _ui_menuDown(uint8_t menu_entries)
         ui_state.menu_selected += 1;
     else
         ui_state.menu_selected = 0;
+    vp_playMenuBeepIfNeeded(ui_state.menu_selected==0);
 }
 
 static void _ui_menuBack(uint8_t prev_state)
@@ -1000,6 +1002,7 @@ static void _ui_menuBack(uint8_t prev_state)
         state.ui_screen = prev_state;
         // Reset menu selection
         ui_state.menu_selected = 0;
+        vp_playMenuBeepIfNeeded(true);
     }
 }
 
